562f30b6a49e8cdc14b1d7d1034822270a9ca4c3
Adam Stubbs <adamstrench2000@gmail.com>
2024-10-24 16:01:44 +0100

    modified:   server2.mjs

--- server2.mjs
+++ server2.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const webhookURL = "https://hooks.slack.com/triggers/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"
 let tempRequest;
 
@@ -15,24 +15,22 @@ app.post('/', function (req, res) {
     JSON.stringify(req.body, null, 4);    // stringify with 4 spaces at each level
     let responseObject = {"text": JSON.stringify(req.body, null, "\t")};
     tempRequest = responseObject;
-    console.log(responseObject);
+    console.log("POST Request received");
     fetch(webhookURL, {
     method: "POST",
     body: JSON.stringify(tempRequest),
     headers: {
         "Content-type": "application/json; charset=UTF-8"
-  }
+    }
 })
-    res.send(200, responseObject);
+    res.status(200).send("Sending to Slack");
 
 });
 
 app.get('/', function (req, res) {
-    console.log(req.body);
-    res.send(200);
+    console.log("GET Request received");
+    res.status(404).send("This endpoint only supports POST requests.");
 });
-
-
 
 app.listen(PORT, function (err) {
     if (err) console.log(err);
